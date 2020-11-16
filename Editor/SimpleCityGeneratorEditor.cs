f95e6c7d373842fa0b25d4ca74662c6ca3794738
Samuel Knox <47692324+SamJCKnox@users.noreply.github.com>
2020-11-16 10:55:56 +0000

    Inital Update

---
+++ Editor/SimpleCityGeneratorEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.XR.ARFoundation;
+using Samspace;
+namespace Samspace
+{
+    [CustomEditor(typeof(SimpleCityGenerator))]
+    public class SimpleCityGeneratorEditor : Editor
+    {
+
+        //public static bool OrginChanger = false;
+
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            SimpleCityGenerator spawner = (SimpleCityGenerator)target;
+
+            //OrginChanger = EditorGUI.Toggle(new Rect(0, 5, 1, 20), "Move Origin", OrginChanger);
+
+            if (GUILayout.Button("Spawn Objects"))
+            {
+                spawner.EditorSpawn();
+
+            }
+
+            if (GUILayout.Button("Remove Spawns"))
+            {
+                spawner.DestroyAll();
+            }
+
+
+
+        }
+    }
+
+}
